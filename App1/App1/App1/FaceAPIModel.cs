aa15883b4773903f74138ae813b92fd0634b7b8b
Monica <njun658@aucklanduni.ac.nz>
2017-07-24 18:00:45 +1200

    First commit

---
+++ App1/App1/App1/FaceAPIModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class FaceAPIModel
+    {
+        public class FaceRectangle
+        {
+            public int Top { get; set; }
+            public int Left { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+        }
+
+        public class FaceAttributes
+        {                               
+            public string Gender { get; set; }
+            public double Age { get; set; }
+            
+        }
+    }
+
+}
+
